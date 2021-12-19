052c8f48985103fd23fff1a265b56ea9771deaa9
Ksawery Michał Kehl <k.kehl@stud.elka.pw.edu.pl>
2021-12-19 16:13:30 +0100

    Changes by antek - improved detected anomaly

--- detection-rules.py
+++ detection-rules.py
@@ -2,7 +2,9 @@ import re
 import Evtx.Evtx as evtx
 import pyshark
 import yara
-
+import pandas as pd
+from scapy.all import *
+from scapy.layers.inet import IP, TCP
 
 
 def detect_words_yara(filepath):
@@ -146,3 +148,49 @@ def detect_words(file_path):
 
 
 detect_num_of_ips_yara("test_dir/test_pcap.pcap")
+
+
+def detect_anomaly(file_path):
+    scapy_cap = rdpcap(file_path)
+    packet_tab = []
+    for packet in scapy_cap:
+        if TCP in packet:
+            ip_src = ""
+            ip_dst = ""
+            if IP in packet:
+                ip_src = packet[IP].src
+                ip_dst = packet[IP].dst
+
+            tcp_time = packet[TCP].time
+            tcp_sport = packet[TCP].sport
+            tcp_dport = packet[TCP].dport
+            packet_tab.append([str(tcp_time), tcp_sport, tcp_dport, ip_src, ip_dst])
+
+    df = pd.DataFrame(packet_tab, columns=['time', 'src_port', 'dst_port', 'ip_src', 'ip_dst'])
+
+    connections = df.groupby(["dst_port"]).size().rename("amount").reset_index()
+    connections.columns = ['port', 'amount']
+
+    connections2 = df.groupby(["src_port"]).size().rename("amount").reset_index()
+    connections2.columns = ['port', 'amount']
+
+    ports_traffic = pd.concat([connections, connections2]).groupby('port').sum().reset_index()
+
+    ports_traffic = ports_traffic[ports_traffic.amount > 100]
+    ports_traffic.sort_values('amount', ascending=False)
+
+    ports_traffic = ports_traffic[ports_traffic.port < 1024]
+
+    trusted_ports = [80, 443]
+    untrusted_ports = ports_traffic[~ports_traffic.port.isin(trusted_ports)]
+
+    if len(untrusted_ports) > 0:
+        action_alert = "remote"
+        action_block = False
+        description = "Alert - untrusted ports"
+    else:
+        action_alert = None
+        action_block = None
+        description = None
+
+    return action_alert, action_block, description, untrusted_ports
